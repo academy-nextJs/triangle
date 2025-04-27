88a4f3fd74ccd3a5b23f0aeba49093acac7ff66f
baranehhh <167031169+baranehhh@users.noreply.github.com>
2025-04-27 14:37:26 +0330

    comment

--- src/components/LandingContainer/index.tsx
+++ src/components/LandingContainer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection/HeroSection'
 import TopRank from './TopRank/TopRank'
 import VillaRental from './VillaRental/VillaRental'
+import Comments from './Comments/Comments'
 
 const LandingContainer = () => {
   return (
@@ -9,6 +10,7 @@ const LandingContainer = () => {
       <HeroSection/>
       <TopRank/>
       <VillaRental/>
+      <Comments/>
     </div>
   )
 }
