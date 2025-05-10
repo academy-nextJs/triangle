06550a880f47bb53f157257ccb61acb977a4a4f4
baranehhh <167031169+baranehhh@users.noreply.github.com>
2025-05-10 13:03:02 +0330

    fix responsive

--- src/app/(main)/HouseReserve/page.tsx
+++ src/app/(main)/HouseReserve/page.tsx
@@ -48,25 +48,44 @@ const staticCards = [
 
 const HouseReservePage = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-6 w-full h-full">
-      <div className="w-full lg:w-[648px] h-[300px] lg:h-[648px]  mt-6">
+    <div className="flex flex-col xl:flex-row w-full gap-6 xl:px-0">
+      <div className="w-full h-[300px] xl:w-[648px] xl:h-[648px] mt-6">
         <Map />
       </div>
 
-      <div className=" mr-8 w-full max-w-[calc(100%-648px-48px)]">
-        <div className="flex  gap-2 mt-6 mb-4">
+      <div className="w-full xl:mr-8 xl:max-w-[calc(100%-648px-48px)]">
+        <div className="flex flex-col sm:flex-row gap-2 mt-6 mb-4">
           <SearchBar />
           <Filter />
         </div>
-        <ScrollArea className="h-[585px] pr-1 ">
-  <div className="flex flex-wrap gap-4 max-[1474px]:flex-col">
-    {staticCards.map((card, index) => (
-      <div key={index} className="w-[calc(50%-0.5rem)] max-[1474px]:w-full">
-        <TakhfifatCard {...card} />
-      </div>
-    ))}
-  </div>
-</ScrollArea>
+
+        <ScrollArea className="h-[585px] pr-1">
+          <div
+            className="
+      flex 
+      flex-wrap 
+      gap-4 
+      xl:justify-start 
+      max-xl:justify-center 
+      max-[1474px]:flex-col 
+      max-[1474px]:items-center
+    "
+          >
+            {staticCards.map((card, index) => (
+              <div
+                key={index}
+                className="
+          w-full 
+          xl:w-[calc(50%-0.5rem)] 
+          max-[1474px]:w-full 
+          max-xl:w-[calc(50%-0.5rem)]
+        "
+              >
+                <TakhfifatCard {...card} />
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
