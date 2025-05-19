83d3cc78c41ae8ba6b3cbeff07eeb5697bebcd4f
nima <nimaruhi85@gmail.com>
2025-05-19 20:34:54 +0330

    fix

--- src/components/LandingContainer/Categories/index.tsx
+++ src/components/LandingContainer/Categories/index.tsx
@@ -1,71 +1,44 @@
-"use client";
 import { getcategories } from "@/utils/services/api/Landing/Categori/Categori";
-// import { getcategories } from "@/utils/services/api/Landing/Categori/Categori";
-import { da } from "date-fns/locale";
+// Images
+import aparteman from "@/assets/landing/category/aparteman.png";
+import estakhr from "@/assets/landing/category/estakhr.png";
+import kolbe from "@/assets/landing/category/kolbei.png";
+import Villa from "@/assets/landing/category/vilaii.png";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 export interface categories {
   id: string;
   name: string;
 }
-const categories = [
-  { id: "1", name: "استخر دار", image: "/estakhrdar.png" },
-  { id: "2", name: "ساحلی", image: "/saheli.png" },
-  { id: "3", name: "آپارتمانی", image: "/aparteman.png" },
-  { id: "4", name: "ویلا یی", image: "/vilaei.png" },
-  { id: "5", nsme: "بومگردی", image: "/bomgardi.png" },
-  { id: "6", name: "کلبه ای", image: "/colbeei.png" },
-];
 
-export const CategoryGrid = () => {
-  // const [data, setData] = useState([])
+export const CategoryGrid = async () => {
+  const res = await getcategories();
 
-  // useEffect(() => {
-  //   fetch("https://delta-project.liara.run/categories")
-  //     .then(res => res.json())
-  //     .then(setData)
-
-  //   }, [])
-
-  //   console.log(data,"sss");
-
-  const [destinations, setDestinations] = useState<categories[]>([]);
-
-  useEffect(() => {
-    const fetchDestinations = async () => {
-      try {
-        const response = await getcategories();
-        if (Array.isArray(response)) {
-          const data = response.map((item) => ({
-            id: item.id,
-            name: item.name,
-          }));
-          setDestinations(data);
-        }
-      } catch (error) {
-        console.error("Failed to fetch destinations:", error);
-      }
-    };
-    
-    
-    fetchDestinations();
-  }, []);
+  const posts: categories[] = await res;
 
   return (
     <div className=" grid justify-center pt-20 px-4 ">
       <h2 className="text-2xl font-bold text-right mb-6">دسته بندی ها</h2>
       <div className="grid   lg:grid-cols-2 xl:grid-cols-3  gap-4">
-        {categories.map((data) => (
+        {posts.map((data) => (
           <div
             key={data.id}
             className="relative h-[189px] w-[389px] rounded-3xl overflow-hidden shadow-lg group"
           >
             <Image
-              src={data.image}
+              src={
+                data.name == "ویلا"
+                  ? Villa.src
+                  : data.name == "مسکونی"
+                  ? estakhr.src
+                  : data.name == "آپارتمان"
+                  ? aparteman.src
+                  : kolbe.src
+              }
               alt={data.name}
               fill
               className="object-cover transition-transform duration-300 group-hover:scale-105"
             />
+
             <div className="absolute bottom-4 right-4 bg-black/50 text-white text-lg font-bold px-3 py-1 rounded-xl">
               {data.name}
             </div>
--- src/components/LandingContainer/HeroSection/HeroSection.tsx
+++ src/components/LandingContainer/HeroSection/HeroSection.tsx
@@ -1,38 +1,13 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Input } from "@/components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { format } from "date-fns";
+
 import { LucideArrowLeft } from "lucide-react";
-import { RxCalendar } from "react-icons/rx";
 import epartman from "../../../../public/eparteman.png";
 import Link from "next/link";
-import { useState } from "react";
 import Image from "next/image";
 import { BlurFade } from "@/components/magicui/BlurFade";
+import { Filter } from "./Filter";
 
 const HeroSection = () => {
-  const [activeTab, setActiveTab] = useState("رزرو ملک");
-  const [startDate, setStartDate] = useState<Date>();
-  const [endDate, setEndDate] = useState<Date>();
-  const [destination, setDestination] = useState<string>("");
-
-  const tabs = ["خرید و فروش", "رهن و اجاره", "رزرو ملک"];
-  const destinations = ["تهران", "مشهد", "اصفهان", "شیراز", "تبریز"];
-
   return (
     <div className="  flex  justify-center ">
       <div className="flex w-[1260px] items-center flex-wrap justify-between  mt-9">
@@ -42,128 +17,11 @@ const HeroSection = () => {
           </BlurFade>
         </div>
         <div className=" mx-auto h-auto absolute lg:flex hidden top-[11%] transform -translate-y-1/2 px-4">
-        <BlurFade delay={0.3} inView>
-          <div className="flex flex-col items-center w-full">
-            <div className="w-full dark:bg-[#020618] bg-white rounded-2xl shadow-md p-4 md:p-6 flex flex-col gap-6">
-              {/* Tabs */}
-              <div className="flex gap-4 justify-center md:justify-end pt-[1px] pr-0 md:pr-8 flex-wrap">
-                {tabs.map((tab) => (
-                  <div key={tab} className="flex flex-col items-center">
-                    <div
-                      className={`h-1.5 w-full rounded-b-lg ${
-                        activeTab === tab ? "bg-blue-600" : "bg-transparent"
-                      } transition-all`}
-                    />
-                    <button
-                      onClick={() => setActiveTab(tab)}
-                      className={`px-4 py-2 text-[16px] font-medium dark:bg-[#020618] dark:text-gray-200  bg-white transition-all font-[IranYekanRegular] ${
-                        activeTab === tab
-                          ? "text-blue-600 bg-blue-100"
-                          : "text-gray-600 bg-gray-100"
-                      }`}
-                    >
-                      {tab}
-                    </button>
-                  </div>
-                ))}
-              </div>
-
-              {/* Form Fields */}
-              <div className="flex flex-nowrap gap-4 md:gap-6 items-center justify-center md:justify-start">
-                {/* مشاهده نتیجه */}
-                <Button className="h-[48px] px-6 bg-blue-600 text-white dark:text-gray-200  hover:bg-blue-700 whitespace-nowrap font-[IranYekanRegular]">
-                  مشاهده نتیجه
-                </Button>
-
-                {/* تاریخ خروج */}
-                <div className="flex items-center gap-2 relative">
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className=" w-[122px] xl:w-[162px] h-[48px] justify-start pl-10 pr-3 relative text-gray-400 dark:text-gray-200 font-[IranYekanRegular]"
-                      >
-                        <RxCalendar className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-200  font-[IranYekanRegular]" />
-                        <span className=" text-[12px] xl:text-[14px] dark:text-gray-200 font-[IranYekanRegular] ">
-                          {endDate
-                            ? format(endDate, "yyyy/MM/dd")
-                            : "انتخاب تاریخ"}
-                        </span>
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        selected={endDate}
-                        onSelect={setEndDate}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                  <label className="text-gray-700  text-[12px] xl:text-[14px] font-medium dark:text-gray-200  min-w-max font-[IranYekanRegular]">
-                    :تاریخ خروج
-                  </label>
-                </div>
-
-                <div className="flex items-center gap-2 relative">
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className=" w-[122px]  xl:w-[162px] h-[48px] justify-start pl-10 pr-3 relative dark:text-gray-200  text-gray-400"
-                      >
-                        <RxCalendar className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-200 " />
-                        <span className=" text-[12px] xl:text-[14px] text-right dark:text-gray-200 font-[IranYekanRegular]">
-                          {startDate
-                            ? format(startDate, "yyyy/MM/dd")
-                            : "انتخاب تاریخ"}
-                        </span>
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        selected={startDate}
-                        onSelect={setStartDate}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                  <label className="text-gray-700  text-[12px] xl:text-[14px] font-medium min-w-max dark:text-gray-200 font-[IranYekanRegular]">
-                    :تاریخ ورود
-                  </label>
-                </div>
-
-                {/* تعداد نفرات */}
-                <div className="flex items-center gap-2">
-                  <Input placeholder="تعداد" className=" w-[122px] xl:w-[162px] h-[48px] font-[IranYekanRegular]" />
-                  <label className="text-gray-700  text-[12px] xl:text-[14px] font-medium min-w-max dark:text-gray-200 font-[IranYekanRegular]">
-                    :تعداد نفرات
-                  </label>
-                </div>
-
-                {/* انتخاب مقصد */}
-                <div className="flex items-center gap-2">
-                  <Select onValueChange={(value) => setDestination(value)}>
-                    <SelectTrigger className=" w-[122px] xl:w-[162px] h-[48px] font-[IranYekanRegular] ">
-                      <SelectValue placeholder="مقصد" className="dark:text-gray-200 font-[IranYekanRegular] " />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {destinations.map((city) => (
-                        <SelectItem key={city} value={city}>
-                          {city}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                  <label className="text-gray-700  text-[12px] xl:text-[14px] font-medium min-w-max dark:text-gray-200 font-[IranYekanRegular]">
-                    :انتخاب مقصد
-                  </label>
-                </div>
-              </div>
+          <BlurFade delay={0.3} inView>
+            <div className="flex flex-col items-center w-full">
+              <Filter />
             </div>
-          </div>
-        </BlurFade>
+          </BlurFade>
         </div>
 
         <div className="flax justify-items-end">
